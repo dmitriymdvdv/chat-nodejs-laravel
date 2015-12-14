a2f2f1d27087340fcaae9dc92f0d819b45df2350
Vasilevich Artem <formulaedm@gmail.com>
2015-12-14 19:08:08 +0300

    SL-40 - changes

--- frontend/local.js
+++ frontend/local.js
@@ -9,36 +9,51 @@ var http = require('http').Server(app);
 var io = require('socket.io')(http);
 var Redis = require('ioredis');
 var currentRoom = {};
+var sideBarChatsId = [];
 
 app.set('port', process.env.PORT || 3020);
 app.set('env', process.env.NODE_ENV || 'local');
 
-var server = http.listen(app.get('port'), function() {
-    console.log('phplabs web server listening on port ' + server.address().port);
+var server = http.listen(app.get('port'), function () {
+    console.log('web server listening on port ' + server.address().port);
 });
 
-io.on('connection', function(socket){
+io.on('connection', function (socket) {
     var redis = new Redis(6379);
 
-    console.log('user connect', socket.id);
-
-    socket.on('join', function(room) {
+    socket.on('join', function (room) {
         currentRoom[socket.id] = room.chatId;
+    });
+    socket.on('join-to-all', function (rooms) {
+        sideBarChatsId = rooms;
     });
 
     redis.subscribe('message-channel');
 
-    redis.on('message', function(channel, message) {
+    redis.on('message', function (channel, message) {
+
         message = JSON.parse(message);
+
+        if (idExists(message['chat_id'])) {
+            socket.emit('notify', Number(message['chat_id']));
+        }
+
         if (currentRoom[socket.id] === message['chat_id']) {
             socket.json.send(message);
         }
     });
 
-    socket.on('disconnect', function(){
-        console.log('user disconnect', socket.id);
+    socket.on('disconnect', function () {
         socket.leave(currentRoom[socket.id]);
         redis.disconnect();
-        console.log('user disconnected');
     });
+
+    function idExists(currentChat) {
+        for (var i = 0; i < sideBarChatsId.length; i++) {
+            if (sideBarChatsId[i] == Number(currentChat)) {
+                return true;
+            }
+        }
+        return false;
+    }
 });
